b5637f48b621e4fba9951e406d688294a97aaa1f
Tony McKenzie <mckenzie.tony@gmail.com>
2021-04-27 23:15:16 -0700

    iam resource fix

--- 333153126918_prod/Terraform/trader_app/iam.tf
+++ 333153126918_prod/Terraform/trader_app/iam.tf
@@ -19,6 +19,12 @@ resource "aws_iam_role_policy_attachment" "ecs_agent" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
+# amazonecs_fullaccess needs to be attached - syntax might need correcting
+resource "aws_iam_role_policy_attachment" "ecs_agent" {
+  role       = aws_iam_role.ecs_agent.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECS_FullAccess"
+}
+
 resource "aws_iam_instance_profile" "ecs_agent" {
   name = "ecs-agent"
   role = aws_iam_role.ecs_agent.name
--- 333153126918_prod/Terraform/trader_app/sg.tf
+++ 333153126918_prod/Terraform/trader_app/sg.tf
@@ -7,4 +7,11 @@ resource "aws_security_group" "ecs_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
