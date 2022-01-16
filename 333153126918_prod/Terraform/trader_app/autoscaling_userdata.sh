a50c296a382dbf583ce32067b194c680912f480b
Tony McKenzie <mckenzie.tony@gmail.com>
2022-01-15 16:24:14 -0800

    removing outdated ssh pub keys

--- 333153126918_prod/Terraform/trader_app/autoscaling_userdata.sh
+++ 333153126918_prod/Terraform/trader_app/autoscaling_userdata.sh
@@ -19,11 +19,6 @@ echo "Subsystem powershell /usr/bin/pwsh -sshs -NoLogo" >> /etc/ssh/sshd_config
 echo "export DOTNET_CLI_TELEMETRY_OPTOUT=true" >> /home/ec2-user/.bashrc
 
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICOt4ZBM8uAg7SkVj4h6iw4dsSPRaC4bg+cm48M4hVmr tmckenzie@vesuvius" >> /home/ec2-user/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzzLckaeONWN6iFD4AV4sNsfYhvLUEIEPNcZwhlWBIjMfwD4lVo+k7Z0uer272eXTOUd5D6CVN1wtK4ywpOGbJEFdfrqHxVe405vhvatHA1FJI7kM8Gz5sIz2HcVebW2hlTv2xmipbgleEq4YRAf5JcL6s56E+OG+bfEq9pQD1H2sbMBrq+oxxA5M/iae+t2evTU8OTfoYhNX0Fv3H3sbkCIj1YGGwIHjAlSk46EGGCdYAvPyODkDfwhR6cMt10C5zGG5Lw50dejGV4Nll0nwZpygbrrEYw/rbAVyU+unr6SnaPWkF+9QCIp0kSFfAtzr2Vf1Yd9ZmTJPlse5d1yov james-test-key-nrt" >> /home/ec2-user/.ssh/authorized_keys
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKGKbJ6HuDz5iiY24qi1vCx8lMI9/xmeJrR9kqLCINip james@cantor" >> /home/ec2-user/.ssh/authorized_keys
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILEin9slpQoj+hECOPrFTdQKu516Wb1EmSNuI6uzZ0DX james@laplace" >> /home/ec2-user/.ssh/authorized_keys
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKGKbJ6HuDz5iiY24qi1vCx8lMI9/xmeJrR9kqLCINip james@cantor" >> /home/ec2-user/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDb/bdXMbUYuMVsqpUSFDWAMWNRw0mVupV1+tmdz4ePuIBUnxOA1wqA6WTcWDJaCTk27iZmiWVv8ylrvJeQvwWnQzahUluqdUkTwz3MWAFK9/J3sbrS5PAD1HGzz/Pg0+jNoJv9r76veOKPoFKwgCB0BNahmZllcjOknzEfIaV/XDYgBNEV2mk13ODEUK962CObJFcweb+914RfOL72e10hu9Hy2F4HjIOMrMKLN22cQCJVK9vlMSJSuthSXyBeMAXRSZKn1IQ2/W9moRg9CXzDnbJLHT3+gxjh8zIRAoFN8AI9JyoNB0LS0z/rssYzTayCzhYAHtmMx4uT+u9bRJZ7kOawOaPIvd61KFUEYdiPkvhegLcXTpNnnSC4baj6hrMR3QbPKgTV43UDuhjtNDxVsp62laVPccE/KjXgalww2g1GNG/pubS1dCw3d/YSF9/icjL1iIscs4/CR0HqgYGIiZVgXGEp6SMKrICROxK3J99zJCT1PS6BCMOxlXgpS3M= user@Primary" >> /home/ec2-user/.ssh/authorized_keys
 systemctl restart sshd
 
 # configure system performance profile and return result
