17771cc32a0acb3127227d35489b84671f3e6bc7
Tony McKenzie <mckenzie.tony@gmail.com>
2022-01-15 22:04:57 -0800

    add docker example

--- 240840959695_test/Terraform/master/org_master.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_organizations_organization" "master" {
-  aws_service_access_principals = [
-    "cloudtrail.amazonaws.com",
-    "config.amazonaws.com",
-  ]
-
-  feature_set = "ALL"
-}
--- 240840959695_test/Terraform/trader_app/task_definition.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecs_task_definition" "task_definition" {
-  family                = "worker"
-  container_definitions = data.template_file.task_definition_template.rendered
-}
